ffcc2d4e02a35f60a6e09562b5e354616d669af1
Braden.Scothern <DanceToasterLLC@gmail.com>
2016-08-31 20:40:14 -0600

    agent_1 should be done

--- Assignment 1/Code/CS4300_agent1.m
+++ Assignment 1/Code/CS4300_agent1.m
@@ -21,25 +21,89 @@ board = [
     0 0 1 0;
     0 0 0 1;
     0 1 0 3;
-    0 1 0 0]
+    0 1 0 0];
 
 % face can be set to one of these in order to know which way the player is facing
-RIGHT = [1 0];
-LEFT = [-1 0];
-UP = [0 1];
-DOWN = [0 -1];
+RIGHT_step = [1 0];
+LEFT_step = [-1 0];
+UP_step = [0 1];
+DOWN_step = [0 -1];
+
+RIGHT = 1;
+LEFT = 2;
+UP = 3;
+DOWN = 4;
 
 % the valid values of face are defined above, add face to pos in order to move that direction.
 %This doens't make sure that it is valid so that must be checked afterwards
-face = RIGHT
+face = RIGHT;
 
 % the current location
-pos = [1 1]
+pos = [1 1];
 
-steps = 0
-res = 0
+steps = 0;
+res = 0;
 
 while res == 0
+    action = randi(3);
+    switch action
+        case 1  % step forward
+            % get the new location
+            dir = [0 0];
+            switch face
+                case RIGHT
+                    dir = RIGHT_step;
+                case LEFT
+                    dir = LEFT_step;
+                case UP
+                    dir = UP_step;
+                case DOWN
+                    dir = DOWN_step;
+            end
+            potential_pos = pos + dir;
+            
+            % if this check fails then we are bouncing off the wall
+            if ~(potential_pos(1) < 1 || potential_pos(1) > 4 || potential_pos(2) < 1 || potential_pos(2) > 4) == 1
+                pos = potential_pos;
+                steps = steps + 1;
+
+                % check to see what happened with the step
+                step_res = board(pos(1), pos(2));
+                switch step_res
+                    case 1
+                        res = 1;
+                        return;
+                    case 2
+                        res = 2;
+                        return;
+                    case 3
+                        res = 3;
+                        return;
+                end
+            end
+        case 2  % turn left
+            if face == RIGHT
+                face = UP;
+            elseif face == LEFT
+                face = DOWN;
+            elseif face == UP
+                face = LEFT;
+            elseif face == DOWN
+                face = RIGHT;
+            end
+
+        case 3  % turn right
+            switch face
+                case RIGHT
+                    face = DOWN;
+                case LEFT
+                    face = UP;
+                case UP
+                    face = RIGHT;
+                case DOWN
+                    face = LEFT;
+            end
+    end
 end
 
 end
