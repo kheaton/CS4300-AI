9b9564aa8e22fc88b3dfaa6407b3830fbeadde78
Braden.Scothern <DanceToasterLLC@gmail.com>
2016-09-07 23:21:11 -0600

    added the second part of queue logic in A*

--- Assignment 2/Code/CS4300_Wumpus_A_star1.m
+++ Assignment 2/Code/CS4300_Wumpus_A_star1.m
@@ -127,7 +127,18 @@ while 1==1
                 end
             end
         elseif option == 2
-            
+            for i = 1:size(frontier, 2)
+                if i == size(frontier, 2)
+                    frontier = [frontier(1:end), next_list(1,1)];
+                elseif next_list(1,2) < frontier(i)
+                    if i == 1
+                        frontier = [next_list(1,1), frontier(1:end)];
+                    else
+                        frontier = [frontier(1:(i - 1)), next_list(1,1), frontier(i,end)];
+                    end
+                    break
+                end
+            end
         end
         % remove the first element that we just worked with
         next_list = next_list(2:end, :)
